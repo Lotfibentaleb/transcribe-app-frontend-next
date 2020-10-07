9559ce5aa4dc5830e6aa572b7e129949b6033b29
Lotfi <lotfi.bentaleb1995@gmail.com>
2020-10-07 07:58:04 +0200

    Change sidebar filter color.

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -31,7 +31,7 @@ export default function Admin({ children, ...rest }) {
   const mainPanel = React.createRef();
   // states and functions
   const [image, setImage] = React.useState(bgImage);
-  const [color, setColor] = React.useState("white");
+  const [color, setColor] = React.useState("purple");
   const [fixedClasses, setFixedClasses] = React.useState("dropdown");
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const handleImageClick = (image) => {
